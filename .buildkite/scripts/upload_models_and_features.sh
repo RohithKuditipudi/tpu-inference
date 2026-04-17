7dddbbe6c61bbbacaa56aa3ad744611655bbdcb1
dennis yeh <dennis.yeh@cienet.com>
2026-04-17 10:16:01 +0800

    test excluded yml
    Signed-off-by: dennis yeh <dennis.yeh@cienet.com>

--- .buildkite/scripts/upload_models_and_features.sh
+++ .buildkite/scripts/upload_models_and_features.sh
@@ -21,25 +21,26 @@ MODEL_LIST_KEY="model-list"
 FEATURE_LIST_KEY="feature-list"
 
 declare -a TARGET_FOLDERS=(
-    "quantization"
-    "parallelism"
-    "models"
-    "features"
-    "rl"
+    "excluded"
+    # "quantization"
+    # "parallelism"
+    # "models"
+    # "features"
+    # "rl"
 )
 
 
 # Use find to append the kernel_microbenchmarks subdirectories
 KERNEL_PARENT_DIR=".buildkite/kernel_microbenchmarks"
 
-if [[ -d "$KERNEL_PARENT_DIR" ]]; then
-    while IFS= read -r dir; do
-        folder_path_to_add="${dir#"${BUILDKITE_DIR}"/}"
-        TARGET_FOLDERS+=("$folder_path_to_add")
-    done < <(find "$KERNEL_PARENT_DIR" -maxdepth 1 -mindepth 1 -type d)
-else
-    echo "Warning: Kernel microbenchmarks directory '$KERNEL_PARENT_DIR' not found. Skipping dynamic folder discovery."
-fi
+# if [[ -d "$KERNEL_PARENT_DIR" ]]; then
+#     while IFS= read -r dir; do
+#         folder_path_to_add="${dir#"${BUILDKITE_DIR}"/}"
+#         TARGET_FOLDERS+=("$folder_path_to_add")
+#     done < <(find "$KERNEL_PARENT_DIR" -maxdepth 1 -mindepth 1 -type d)
+# else
+#     echo "Warning: Kernel microbenchmarks directory '$KERNEL_PARENT_DIR' not found. Skipping dynamic folder discovery."
+# fi
 
 # Arrays to store YAML content fragments (without 'steps:' header)
 pipeline_v6e_fragments=()
@@ -76,6 +77,9 @@ for folder_path in "${TARGET_FOLDERS[@]}"; do
 
       case "$folder_path" in
         "models")
+          model_list+=("$subject_name")
+          ;;
+        "excluded")
           model_list+=("$subject_name")
           ;;
         "features" | "parallelism" | "quantization" | "kernel_microbenchmarks"/* | "rl")
